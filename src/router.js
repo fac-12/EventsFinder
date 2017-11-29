8d0ee288cfd306c719d46e093722e584829736e2
Mohamed Osman <trc5019@gmail.com>
2017-11-29 15:27:15 +0000

    Update router.js

--- src/router.js
+++ src/router.js
@@ -9,7 +9,6 @@ const router = (request, response) => {
     handlers.staticFileHandler(request, response, endpoint);
   }
   else if (endpoint.indexOf('search') !== -1) {
-    console.log(request);
     handlers.searchHandler(request, response, endpoint);
   }
   else if (endpoint.indexOf('add-event') !== -1) {
