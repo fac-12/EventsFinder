1c39ee0efa4bb6c6bcca94751ec9cda58033ce67
trc5019 <trc5019@gmail.com>
2017-11-30 10:43:35 +0000

    rewrote db

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -5,15 +5,12 @@ DROP TABLE IF EXISTS events, users, attendance, comments;
 CREATE TABLE events (
     id serial PRIMARY KEY,
     host_name VARCHAR(100) NOT NULL,
-    host_email VARCHAR(100) NOT NULL,
-    host_url VARCHAR(100) NOT NULL,
     venue_name VARCHAR(100) NOT NULL,
     venue_address VARCHAR(100) NOT NULL,
     venue_postcode VARCHAR(10) NOT NULL,
     event_name VARCHAR(100) NOT NULL,
     event_date DATE NOT NULL,
     event_start_time TIME NOT NULL,
-    event_end_time TIME,
     event_url VARCHAR(100)
 );
 
--- src/database/db_testdata.sql
+++ src/database/db_testdata.sql
@@ -1,19 +1,10 @@
 BEGIN;
 
-INSERT INTO events(host_name, host_url, host_email) VALUES
-    ('Founders and Coders', 'http://www.foundersandcoders.com', 'hello@foundersandcoders.com'),
-    ('ReactJS Girls London', 'https://www.meetup.com/ReactJS-Girls-London', '');
-
-INSERT INTO events(venue_name, venue_address, venue_postcode) VALUES
-    ('Founders and Coders', '14 Palmers Road, London', 'E2 0SY'),
-    ('Space4', '4 Fonthill Rd, 1st Floor, London', 'N4 3HF'),
-    ('Facebook Headquarters', '10 Brock St, Kings Cross', 'NW1 3FG');
-
-INSERT INTO events(event_name, event_date, event_start_time, event_end_time, event_url) VALUES
-    ('Coding for Everyone', '04/12/2017', '18:30', '20:00', 'https://www.meetup.com/founderscoders/events/nwkhxnywqbgb/'),
-    ('Coding for Everyone', '04/12/2017', '18:30', '20:00', 'https://www.meetup.com/founderscoders/events/nwkhxnywqbgb/'),
-    ('Coding for Women', '12/12/2017', '18:30', '20:00','https://www.meetup.com/founderscoders/events/nwkhxnywqbgb/'),
-    ('ReactJS Girls #4 - From React to React Native: How Hard Can It Be?', '12/12/2017', '18:15', '20:30','');
+INSERT INTO events(host_name, venue_name, venue_address, venue_postcode, event_name, event_date, event_start_time, event_url) VALUES
+    ('Founders and Coders', 'Founders and Coders', '14 Palmers Road, London', 'E2 0SY','Coding for Everyone', '04/12/2017', '18:30', 'https://www.meetup.com/founderscoders/events/nwkhxnywqbgb/'),
+    ('Founders and Coders', 'Space4', '4 Fonthill Rd, 1st Floor, London', 'N4 3HF','Coding for Everyone', '04/12/2017', '18:30', 'https://www.meetup.com/founderscoders/events/nwkhxnywqbgb/'),
+    ('Founders and Coders', 'Founders and Coders', '14 Palmers Road, London', 'E2 0SY','Coding for Women', '04/12/2017', '18:30', 'https://www.meetup.com/founderscoders/events/nwkhxnywqbgb/'),
+    ('ReactJS Girls London', 'Facebook Headquarters', '10 Brock St, Kings Cross', 'NW1 3FG','ReactJS Girls #4 - From React to React Native: How Hard Can It Be?', '12/12/2017', '18:15',''),
 
 INSERT INTO users(first_name, last_name, email) VALUES
     ('Shannon', 'W', 'shannon@gmail.com'),
